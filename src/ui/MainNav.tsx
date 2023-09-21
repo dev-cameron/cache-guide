961528fcb1522ee4acefc1dfbc86efa42b3ae241
dev-cameron <gccmckinley@gmail.com>
2023-09-21 14:39:11 -0700

    add next-themes for dark mode management

--- src/ui/MainNav.tsx
+++ src/ui/MainNav.tsx
@@ -5,10 +5,12 @@ import { items } from "@/lib/items";
 import Link from "next/link";
 import { usePathname } from 'next/navigation';
 import clsx from "clsx";
+import { useTheme } from 'next-themes';
 
 export default function MainNav() {
   const [isOpen, setIsOpen] = useState(false);
-  const [theme, setTheme] = useState('light')
+  // const [theme, setTheme] = useState('light')
+  const { theme, setTheme } = useTheme();
 
   const pathname = usePathname();
 
@@ -38,10 +40,10 @@ export default function MainNav() {
   )});
 
   const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light')
-    const html: HTMLHtmlElement | null = document.querySelector('html')
-    if (html){
-      html.className = theme;
+    if (theme === 'light') {
+      setTheme('dark')
+    } else {
+      setTheme('light')
     }
   }
 
@@ -92,7 +94,7 @@ export default function MainNav() {
         <button className="absolute bottom-4 left-4 rounded-lg shadow-inner bg-neutral-accent hover:bg-neutral text-white px-3 py-1"
           onClick={()=>toggleTheme()}
         >
-          { theme === 'light' ? (
+          { theme === 'dark' ? (
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
             </svg>
